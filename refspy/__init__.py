b64eec4cb93146666f5acf6e7a25adc38779e2ea
Nigel Chapman <nigel@chapman.id.au>
2024-06-29 21:01:39 +1000

    Moved refspy() to __init__ in module

--- refspy/__init__.py
+++ refspy/__init__.py
@@ -1,1 +1,6 @@
-# Package
+from refspy.manager import Manager
+from refspy.setup import get_libraries, get_language
+
+
+def refspy(corpus="protestant", locale="en_US"):
+    return Manager(get_libraries(corpus, locale), get_language(locale[:2]))
