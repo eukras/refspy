bd3eaeea1d16618127766be55cd20d48fc1717b1
Rick Brannan <43759793+RickBrannan@users.noreply.github.com>
2025-02-26 18:30:09 -0800

    Add spanish support (OT, NT) to refspy

--- refspy/config.py
+++ refspy/config.py
@@ -4,13 +4,15 @@ from typing import Dict, List
 
 from refspy.language import Language
 from refspy.languages.english import ENGLISH
+from refspy.languages.spanish import SPANISH
 from refspy.libraries.en_US import DC, DC_ORTHODOX, NT, OT
+from refspy.libraries.es_ES import DC_es, DC_ORTHODOX_es, NT_es, OT_es
 from refspy.library import Library
 
 LIBRARIES: Dict[str, Dict[str, List[Library]]] = {
-    "protestant": {"en_US": [OT, NT]},
-    "catholic": {"en_US": [OT, DC, NT]},
-    "orthodox": {"en_US": [OT, DC, DC_ORTHODOX, NT]},
+    "protestant": {"en_US": [OT, NT], "es_ES": [OT_es, NT_es]},
+    "catholic": {"en_US": [OT, DC, NT], "es_ES": [OT_es, DC_es, NT_es]},
+    "orthodox": {"en_US": [OT, DC, DC_ORTHODOX, NT], "es_ES": [OT_es, DC_es, DC_ORTHODOX_es, NT_es]},
 }
 """A dictionary of available locales for available libraries, used for
 shorthand library invocation.
@@ -25,6 +27,7 @@ Example:
 
 LANGUAGES: Dict[str, Language] = {
     "en": ENGLISH,
+    "es": SPANISH,
 }
 """A dictionary of available languages, used for shorthand library invocation.
 
