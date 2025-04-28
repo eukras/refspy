6277d92d01c1c912ecc4c9201110c73ef1e857ff
Antoine <antoine@2ohm.fr>
2025-04-28 22:15:29 +0200

    Reference formating is localized

---
+++ refspy/formats/fr_FR.py
@@ -0,0 +1,87 @@
+"""
+Data object for reference formatting options.
+
+The Format objects are used by the `refspy.formatter.Formatter` to turn
+references into different kinds of strings. Properties are named by their
+characters in normal English formatting.
+
+Attributes:
+    book_only: False
+    colon: ':'
+    comma: ','
+    dash: '-'
+    number_only: False
+    property: 'name'
+    semicolon: '; '
+    space: ' '
+
+Note:
+    If book_only is true, number_only is ignored.
+"""
+
+from refspy.format import Format, Formats
+
+CHAR_NBSP = ' '
+
+FORMATS = Formats(
+    NAME_FORMAT = Format(
+        colon=f",{CHAR_NBSP}",
+        comma=".",
+        dash="–",
+        book_only=False,
+        number_only=False,
+        property="name",
+        semicolon="; ",
+        space=" ",
+    ),
+    BOOK_FORMAT = Format(
+        colon=f",{CHAR_NBSP}",
+        comma=".",
+        dash="–",
+        book_only=True,
+        number_only=False,
+        property="name",
+        semicolon="; ",
+        space=" ",
+    ),
+    NUMBER_FORMAT = Format(
+        colon=f",{CHAR_NBSP}",
+        comma=".",
+        dash="–",
+        book_only=False,
+        number_only=True,
+        property=None,
+        semicolon="; ",
+        space=" ",
+    ),
+    ABBREV_NAME_FORMAT = Format(
+        colon=f",{CHAR_NBSP}",
+        comma=".",
+        dash="–",
+        book_only=False,
+        number_only=False,
+        property="abbrev",
+        semicolon="; ",
+        space=" ",
+    ),
+    ABBREV_BOOK_FORMAT = Format(
+        colon=f",{CHAR_NBSP}",
+        comma=".",
+        dash="–",
+        book_only=True,
+        number_only=False,
+        property="abbrev",
+        semicolon="; ",
+        space=" ",
+    ),
+    ABBREV_NUMBER_FORMAT = Format(
+        colon=f",{CHAR_NBSP}",
+        comma=".",
+        dash="–",
+        book_only=False,
+        number_only=True,
+        property="abbrev",
+        semicolon="; ",
+        space=" ",
+    )
+)
