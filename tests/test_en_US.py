bdd4051cee10804e73ba84a6eec50deef937450a
Nigel Chapman <nigel@chapman.id.au>
2025-10-18 22:56:33 +1100

    test: sanity check for deutercanonicals

--- tests/test_en_US.py
+++ tests/test_en_US.py
@@ -1,7 +1,7 @@
 from context import *
 
 from refspy import refspy
-from refspy.libraries.en_US import NT
+from refspy.libraries.en_US import DC, DC_ORTHODOX, NT
 from refspy.reference import verse_reference
 
 __ = refspy()
@@ -31,3 +31,15 @@ def test_numbered_books():
 
     assert __.r("II Cor 1:4") == verse_reference(NT.id, 8, 1, 4)
     assert __.r("II Corinthians 1:4") == verse_reference(NT.id, 8, 1, 4)
+
+
+def test_deuterocanonicals():
+    """
+    Confirm construction of different libraries
+    """
+    catholic = refspy("catholic", "en_US")
+    orthodox = refspy("orthodox", "en_US")
+    assert catholic.r("2 Macc 1:1") == verse_reference(DC.id, 14, 1, 1)
+    assert catholic.r("3 Macc 1:1") is None
+    assert orthodox.r("2 Macc 1:1") == verse_reference(DC.id, 14, 1, 1)
+    assert orthodox.r("3 Macc 1:1") == verse_reference(DC_ORTHODOX.id, 4, 1, 1)
